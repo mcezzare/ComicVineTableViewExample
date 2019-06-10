c8b0b37b4fa0bbb3815f59b535c1152f4e09c78f
Mario Cezzare <mcezzare@gmail.com>
2019-06-10 13:23:24 -0300

    add fetcher class

---
+++ ComicVineTableViewExample/Managers/Fetcher.swift
@@ -0,0 +1,18 @@
+//
+//  Fetcher.swift
+//  ComicVineTableViewExample
+//
+//  Created by Mario Cezzare on 10/06/19.
+//  Copyright © 2019 Mario Cezzare. All rights reserved.
+//
+
+import Foundation
+
+class Fetcher {
+    
+    static let shared = Fetcher()
+    
+    
+    
+    
+}
